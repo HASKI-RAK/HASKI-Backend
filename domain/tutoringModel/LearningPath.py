9086f1ccbc4372451a062d7a974ae262edc8bdb1
AD\e01328 <yeimy.valencia1@outlook.de>
2022-08-19 10:38:42 +0200

    cleanup

--- domain/tutoringModel/LearningPath.py
+++ domain/tutoringModel/LearningPath.py
@@ -1,7 +1,6 @@
-class GenerateLearningPath:
+class LearningPath:
 
     # Learning Element Types: (+) = 1 and (-) = 0
-    # "KU" and "LK":0 Equally relevant for all learners
     learning_style_RQ = {"AKT": 0, "REF": 1, "INT": 1}
     learning_style_SE = {"AKT": 1, "REF": 0, "SNS": 1}
     learning_style_FO = {"AKT": 1, "REF": 0, "INT": 0, "VIS": 0, "VRB": 1}
@@ -9,7 +8,7 @@ class GenerateLearningPath:
                          "SNS": 0, "INT": 1, "VIS": 0, "VRB": 1}
     learning_style_AN = {"AKT": 1, "REF": 0,
                          "SNS": 1, "INT": 0, "VIS": 1, "VRB": 0}
-    learning_style_ÜB = {"AKT": 1, "REF": 0, "SNS": 1, "INT": 1}
+    learning_style_UB = {"AKT": 1, "REF": 0, "SNS": 1, "INT": 1}
     learning_style_BE = {"AKT": 0, "REF": 1, "SNS": 1, "INT": 0, "GLO": 1}
     learning_style_AB = {"SNS": 1, "INT": 0, "GLO": 1}
     learning_style_ZF = {"REF": 1, "GLO": 1}
@@ -18,7 +17,7 @@ class GenerateLearningPath:
         pass
 
     def check_learning_style(self, input_learning_style):
-        # Checks if the input Learning Style is out the range [0-11]
+        
         is_correct = False
 
         for iterator in input_learning_style:
@@ -52,7 +51,7 @@ class GenerateLearningPath:
         learning_style = 0
 
         for learning_style, value in learning_element_types.items():
-            # print ("learning_style",learning_style)
+            
             if (input_learning_style.get(learning_style)):
                 learning_style = input_learning_style.get(learning_style)
 
@@ -70,12 +69,12 @@ class GenerateLearningPath:
 
         for w in sorted_keys:
             sort_learning_path[w] = learning_path[w]
-        
+
         return sort_learning_path
 
     def get_learning_path(self, input_learning_style={"AKT": 0, "INT": 0,
                                                       "VIS": 0, "GLO": 0}):
-        # Calculates the sequence of the learning path
+       
         if (len(input_learning_style) != 4):
             raise ValueError('The Size of Learning Style is not 4')
 
@@ -84,7 +83,6 @@ class GenerateLearningPath:
                 'The Input Learning Style is out the range [0-11]')
 
         LPath = {}
-        
 
         LPath["RQ"] = self.calculate_sequence(
             self.learning_style_RQ, input_learning_style)
@@ -97,7 +95,7 @@ class GenerateLearningPath:
         LPath["AN"] = self.calculate_sequence(
             self.learning_style_AN, input_learning_style)
         LPath["ÜB"] = self.calculate_sequence(
-            self.learning_style_ÜB, input_learning_style)
+            self.learning_style_UB, input_learning_style)
         LPath["BE"] = self.calculate_sequence(
             self.learning_style_BE, input_learning_style)
         LPath["AB"] = self.calculate_sequence(
--- tests/test_learning_path.py
+++ tests/test_learning_path.py
@@ -1,63 +1,64 @@
-import domain.tutoringModel.generate_learning_path as LP
+import domain.tutoringModel.LearningPath as LP
 import pytest
-
-generate_learning_path = LP.GenerateLearningPath()
 
 
 def test_get_learning_path_correct_result():
     input_learning_style = {"AKT": 5, "INT": 9, "VIS": 9, "GLO": 9}
-
+    LearningPath = LP.LearningPath()
     learning_path = {
         'ZF': 99, 'ÜB': 14, 'SE': 5, 'AN': 5,
         'RQ': 4, 'AB': 0, 'ZL': -5, 'BE': -5, 'FO': -13}
-    assert generate_learning_path.get_learning_path(
+    assert LearningPath.get_learning_path(
         input_learning_style) == learning_path
 
 
 def test_get_learning_path_correct_result_2():
     input_learning_style = {"REF": 1, "SNS": 7, "VIS": 5, "SEG": 5}
-
+    LearningPath = LP.LearningPath()
     learning_path = {
         'AN': 11,  'BE': 8, 'AB': 7, 'SE': 6, 'ÜB': 6,
         'RQ': 1, 'ZF': 0, 'FO': -6, 'ZL': -11}
 
-    assert generate_learning_path.get_learning_path(
+    assert LearningPath.get_learning_path(
         input_learning_style) == learning_path
 
 
 def test_get_learning_path_correct_result_3():
 
     input_learning_style = {"AKT": 3, "SNS": 7, "VIS": 3, "GLO": 3}
-
+    LearningPath = LP.LearningPath()
     learning_path = {'ZF': 99, 'AN': 13, 'SE': 10, 'ÜB': 10,
                      'AB': 10, 'BE': 7, 'FO': 0, 'RQ': -3, 'ZL': -13}
-    assert generate_learning_path.get_learning_path(
+    assert LearningPath.get_learning_path(
         input_learning_style) == learning_path
 
 
 def test_get_learning_path_correct_result_4():
 
     input_learning_style = {"AKT": 5, "SNS": 7, "VIS": 7, "GLO": 3}
-
+    LearningPath = LP.LearningPath()
     learning_path = {'ZF': 99, 'AN': 19, 'SE': 12, 'ÜB': 12,
                      'AB': 10, 'BE': 5, 'FO': -2, 'RQ': -5, 'ZL': -19}
-    assert generate_learning_path.get_learning_path(
+    assert LearningPath.get_learning_path(
         input_learning_style) == learning_path
 
 
 def test_get_learning_path_with_empty_learning_style():
+    LearningPath = LP.LearningPath()
     learning_path = {'ZF': 0, 'AN': 0, 'SE': 0, 'ÜB': 0,
                      'AB': 0, 'BE': 0, 'FO': 0, 'RQ': 0, 'ZL': 0}
-    assert generate_learning_path.get_learning_path() == learning_path
+    assert LearningPath.get_learning_path() == learning_path
 
 
 def test_get_learning_path_with_different_size_of_input_learning_style():
+    LearningPath = LP.LearningPath()
     input_learning_style = {"INT": 9, "VIS": 9, "GLO": 9}
     with pytest.raises(ValueError):
-        generate_learning_path.get_learning_path(input_learning_style)
+        LearningPath.get_learning_path(input_learning_style)
 
 
 def test_get_learning_path_with_out_of_Range_input_learning_style_dimension():
+    LearningPath = LP.LearningPath()
     input_learning_style = {"AKT": 12, "INT": 2, "VIS": 11, "GLO": 9}
     with pytest.raises(ValueError):
-        generate_learning_path.get_learning_path(input_learning_style)
+        LearningPath.get_learning_path(input_learning_style)
