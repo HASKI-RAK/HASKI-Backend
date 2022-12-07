f9fe7d3def54a4182b61887a4340991e979d8e39
David Fischer <davidvfischer@gmail.com>
2022-12-07 10:05:04 +0100

    fixed tool config json deleted package init

--- service_layer/lti/config/ToolConfigJson.py
+++ service_layer/lti/config/ToolConfigJson.py
@@ -3,7 +3,7 @@ import os
 
 
 # Parses LTI Config file and returns a dictionary of the contents
-class ToolConfigJson(object):
+class ToolConfigJson():
     _iss_conf_dict : dict = {}
     def __init__(self, config_file="../../../config/lti_config.json"):
         if not os.path.isfile(config_file):
@@ -11,7 +11,6 @@ class ToolConfigJson(object):
 
         with open(config_file, 'r') as cfg:
             self._iss_conf_dict = json.loads(cfg.read())
-            super(ToolConfigJson, self).__init__()
         
     def get_platform(self, iss, client_id=None):
         return self._iss_conf_dict[iss]
