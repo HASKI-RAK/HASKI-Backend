5b80ef6ffc5f46d7c25ced7dc0b4eb13584c962f
MarcNor <marc.normann@rz-online.de>
2023-02-24 13:38:38 +0100

    Updated for codestyle

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ def get_postgres_uri():
     host = os.environ.get("DB_HOST", "localhost")
     port = os.environ.get("DB_PORT", 0)
     password = os.environ.get("DB_PASSWORD", "")
-    user, db_name = os.environ.get("DB_USER", ""), os.environ.get("DB_NAME", "")
+    user = os.environ.get("DB_USER", "")
+    db_name = os.environ.get("DB_NAME", "")
     return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
 
 
