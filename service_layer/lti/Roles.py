a4bc23d9c5a43af8678fb74401332795aecd1982
David Fischer <davidvfischer@gmail.com>
2023-06-23 14:37:13 +0200

    added roles mapping to permissions

--- service_layer/lti/Roles.py
+++ service_layer/lti/Roles.py
@@ -1,9 +1,17 @@
+from utils.auth.permissions import Permissions
+
 
 # This file contains the mapping of IMS roles to LTI / LMS roles
 lti_roles = {
     'http://purl.imsglobal.org/vocab/lis/v2/membership#Instructor': 'Instructor',
     'http://purl.imsglobal.org/vocab/lis/v2/membership#Learner': 'Learner',
     'http://purl.imsglobal.org/vocab/lis/v2/membership#Administrator': 'Administrator'
+}
+
+lti_permissions = {
+    'Instructor': [Permissions.READ, Permissions.WRITE],
+    'Learner': [Permissions.READ],
+    'Administrator': [Permissions.READ, Permissions.WRITE, Permissions.ADMIN]
 }
 
 
@@ -17,13 +25,13 @@ class RoleMapper():
 
     # map IMS roles to LTI roles
     @staticmethod
-    def map_role(role : str):
+    def map_role(role: str):
         if role in lti_roles:
             return lti_roles[role]
         return "Unknown"
-    
-    def __init__(self, roles_list : list[str]):
-        self.role : str = "Unknown"
+
+    def __init__(self, roles_list: list[str]):
+        self.role: str = "Unknown"
         for role in roles_list:
             if RoleMapper.map_role(role) != "Unknown":
                 self.role = RoleMapper.map_role(role)
@@ -31,3 +39,6 @@ class RoleMapper():
 
     def get_role(self):
         return self.role
+
+    def get_permissions(self) -> list[Permissions]:
+        return lti_permissions[self.role]
