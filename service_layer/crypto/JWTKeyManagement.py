3a5132964eefcbce01a05d7e88251a066b79cea2
theUpsider <davidvfischer@gmail.com>
2023-06-26 15:54:55 +0200

    fixed tests deleted launchdatastorage since its replaced with session storage

--- service_layer/crypto/JWTKeyManagement.py
+++ service_layer/crypto/JWTKeyManagement.py
@@ -5,97 +5,140 @@ import pickle
 from typing import Any, Mapping
 from jose import JWSError, jws, jwk
 from jose.backends.base import Key
-from cryptography.hazmat.primitives import serialization as crypto_serialization
+from cryptography.hazmat.primitives import (
+    serialization as crypto_serialization,
+)
 from errors.errors import InvalidJWTError
 from config import get_project_root
-
-from service_layer.crypto.cryptorandom import CryptoRandom
-from service_layer.lti import LaunchDataStorage
 import service_layer.service.SessionServiceFlask as SessionServiceFlask
 
-private_key_location : str = os.path.join(get_project_root(),"keys/private.pem")
-public_key_location : str = os.path.join(get_project_root(),"keys/public.pem")
+private_key_location: str = os.path.join(
+    get_project_root(), "keys/private.pem"
+)
+public_key_location: str = os.path.join(get_project_root(), "keys/public.pem")
+
 
 def load_public_key():
     with open(os.path.abspath(public_key_location), "rb") as key_file:
         public_key = crypto_serialization.load_pem_public_key(key_file.read())
-        return public_key.public_bytes(crypto_serialization.Encoding.PEM,
-                            crypto_serialization.PublicFormat.SubjectPublicKeyInfo)
+        return public_key.public_bytes(
+            crypto_serialization.Encoding.PEM,
+            crypto_serialization.PublicFormat.SubjectPublicKeyInfo,
+        )
 
-def construct_key(key : str | bytes | dict[str, Any] | Key):
+
+def construct_key(key: str | bytes | dict[str, Any] | Key):
     return jwk.construct(key)
 
-def verify_jwt(jwt_token : str, key : str | bytes | Mapping[str, Any] | Key = load_public_key().decode()):
-    ''' Returns the payload of the JWT token if it is valid, otherwise raises an exception'''
+
+def verify_jwt(
+    jwt_token: str,
+    key: str | bytes | Mapping[str, Any] | Key = load_public_key().decode(),
+):
+    """Returns the payload of the JWT token if it is valid, otherwise raises an exception"""
     try:
-        return json.loads(jws.verify(jwt_token, key, algorithms=["RS256"]).decode('UTF-8'))
+        return json.loads(
+            jws.verify(jwt_token, key, algorithms=["RS256"]).decode("UTF-8")
+        )
     except JWSError as e:
         raise InvalidJWTError(e)
 
-def get_unverified_header(jwt_token : str):
+
+def get_unverified_header(jwt_token: str):
     try:
         return jws.get_unverified_header(jwt_token)
     except JWSError as e:
         raise InvalidJWTError(e)
 
-def sign_jwt(payload : dict):
+
+def sign_jwt(payload: dict):
     with open(os.path.abspath(private_key_location), "rb") as key_file:
-            private_key = crypto_serialization.load_pem_private_key(key_file.read(), password=None)
+        private_key = crypto_serialization.load_pem_private_key(
+            key_file.read(), password=None
+        )
 
-            key_private = private_key.private_bytes(crypto_serialization.Encoding.PEM,
-                            crypto_serialization.PrivateFormat.PKCS8,
-                            crypto_serialization.NoEncryption())
-            return jws.sign(payload, key_private, algorithm="RS256")
+        key_private = private_key.private_bytes(
+            crypto_serialization.Encoding.PEM,
+            crypto_serialization.PrivateFormat.PKCS8,
+            crypto_serialization.NoEncryption(),
+        )
+        return jws.sign(payload, key_private, algorithm="RS256")
 
-def load_jwt(jwt_token : str):
+
+def load_jwt(jwt_token: str):
     return json.loads(jws.get_unverified_claims(jwt_token))
 
-def generate_nonce_jwt(nonce : str, audience : str, issuer : str):
+
+def generate_nonce_jwt(nonce: str, audience: str, issuer: str):
     nonce_jwt = {
-        'nonce': nonce,
-        'iat': datetime.datetime.utcnow().timestamp(),
-        'exp': (datetime.datetime.utcnow() + datetime.timedelta(minutes=60)).timestamp(),
-        'aud': audience,
-        'kid': 'backendprivatekey',
-        'iss': issuer
+        "nonce": nonce,
+        "iat": datetime.datetime.utcnow().timestamp(),
+        "exp": (
+            datetime.datetime.utcnow() + datetime.timedelta(minutes=60)
+        ).timestamp(),
+        "aud": audience,
+        "kid": "backendprivatekey",
+        "iss": issuer,
     }
     return sign_jwt(nonce_jwt)
 
-def generate_state_jwt(nonce : str, state : str, audience : str, issuer : str, additional_claims : dict = {}, expiration : int = 60):
+
+def generate_state_jwt(
+    nonce: str,
+    state: str,
+    audience: str,
+    issuer: str,
+    additional_claims: dict = {},
+    expiration: int = 60,
+):
     state_jwt = {
-        'state': state,
-        'nonce': nonce,
+        "state": state,
+        "nonce": nonce,
         **additional_claims,
-        'iat': datetime.datetime.utcnow().timestamp(),
-        'exp': (datetime.datetime.utcnow() + datetime.timedelta(minutes=expiration)).timestamp(),
-        'aud': audience,
-        'kid': 'backendprivatekey',
-        'iss': issuer
+        "iat": datetime.datetime.utcnow().timestamp(),
+        "exp": (
+            datetime.datetime.utcnow() + datetime.timedelta(minutes=expiration)
+        ).timestamp(),
+        "aud": audience,
+        "kid": "backendprivatekey",
+        "iss": issuer,
     }
     return sign_jwt(state_jwt)
 
+
 def verify_jwt_payload(jwt_payload, verify_nonce=True) -> bool:
     """Verifies the payload of a JWT token. Returns True if the payload is valid, otherwise False."""
-    if verify_nonce and not SessionServiceFlask.get(jwt_payload['nonce'],'state'):
+    if verify_nonce and not SessionServiceFlask.get(
+        jwt_payload["nonce"], "state"
+    ):
         return False
     # verify issued at
-    if jwt_payload['iat'] > datetime.datetime.utcnow().timestamp():
+    if jwt_payload["iat"] > datetime.datetime.utcnow().timestamp():
         return False
     # verify expiration
-    if jwt_payload['exp'] < datetime.datetime.utcnow().timestamp():
+    if jwt_payload["exp"] < datetime.datetime.utcnow().timestamp():
         return False
     # verify issuer
-    if jwt_payload['iss'] != os.environ.get('BACKEND_URL', 'https://backend.haski.app'):
+    if jwt_payload["iss"] != os.environ.get(
+        "BACKEND_URL", "https://backend.haski.app"
+    ):
         return False
     # verify kid
-    if jwt_payload['kid'] != 'backendprivatekey':
+    if jwt_payload["kid"] != "backendprivatekey":
         return False
     return True
 
-def verify_state_jwt_payload(state_jwt_payload) -> bool:
-    if verify_jwt_payload(state_jwt_payload) == False:
+
+def verify_state_jwt_payload(
+    state_jwt_payload, verify_nonce=True, session=True
+) -> bool:
+    if verify_jwt_payload(state_jwt_payload, verify_nonce) == False:
         return False
     # verify state in storage
-    if SessionServiceFlask.get(state_jwt_payload['nonce'],'state') != state_jwt_payload['state']:
-        return False
+    if session:
+        if (
+            SessionServiceFlask.get(state_jwt_payload["nonce"], "state")
+            != state_jwt_payload["state"]
+        ):
+            return False
     return True
--- tests/unit/lti/test_JWTKeyManagement.py
+++ tests/unit/lti/test_JWTKeyManagement.py
@@ -1,43 +1,47 @@
-from cryptography.hazmat.primitives import serialization as crypto_serialization
+from cryptography.hazmat.primitives import (
+    serialization as crypto_serialization,
+)
 from service_layer.crypto.cryptorandom import CryptoRandom
-from service_layer.lti import LaunchDataStorage
 import service_layer.crypto.JWTKeyManagement as jwt
+from service_layer.service import SessionServiceFlask
 
 
-private_key_location : str = "keys/private.pem"
-public_key_location : str = "keys/public.pem"
+private_key_location: str = "keys/private.pem"
+public_key_location: str = "keys/public.pem"
 
-#pytest tests\unit\lti\test_JWTKeyManagement.py --cov
+# pytest tests\unit\lti\test_JWTKeyManagement.py --cov
+
 
 def test_get_unverified_header():
     """Test if a unverified header contains the correct information."""
     # Arrange
-    test_dictionary = {'a': 'b'}
+    test_dictionary = {"a": "b"}
 
     # Act
     token = jwt.sign_jwt(test_dictionary)
     header = jwt.get_unverified_header(token)
 
-    #Assert
-    assert header['alg'] == "RS256" and header['typ'] == "JWT"
+    # Assert
+    assert header["alg"] == "RS256" and header["typ"] == "JWT"
+
 
 def test_load_jwt():
     """Test if the unverified claims contains the correct information."""
     # Arrange
-    test_dictionary = {'a': 'b'}
+    test_dictionary = {"a": "b"}
 
     # Act
     token = jwt.sign_jwt(test_dictionary)
     claims = jwt.load_jwt(token)
 
-    #Assert
+    # Assert
     assert claims == test_dictionary
 
 
 def test_sign_verify_jwt():
     """Test if a payload can be signed with a private key and verified with the public key."""
     # Arrange
-    test_dictionary = {'a': 'b'}
+    test_dictionary = {"a": "b"}
 
     # Act
     token = jwt.sign_jwt(test_dictionary)
@@ -46,16 +50,23 @@ def test_sign_verify_jwt():
     # Assert
     assert test_dictionary == jwt.verify_jwt(token, key_public)
 
+
 def test_nonce_jwt():
     """Test if a nonce-token can be generated and verified."""
     # Arrange
     nonce = CryptoRandom().getrandomstring(32)
-    LaunchDataStorage.set_value(nonce, nonce)
-    issuer = 'http://localhost:5000'
-    audience = 'http://localhost:2000'
+    state = CryptoRandom().getrandomstring(32)
+    issuer = "https://backend.haski.app"
+    audience = "http://localhost"
 
     # Act
     token = jwt.generate_nonce_jwt(nonce, audience, issuer)
+    state = jwt.generate_state_jwt(nonce, state, audience, issuer)
+    state_jwt = SessionServiceFlask.set_state_jwt(
+        nonce,
+        "https://moodle.haski.app/mod/lti/auth.php",
+        "https://backend.haski.app",
+    )
     key_public = jwt.load_public_key()
 
     # Assert
@@ -67,15 +78,24 @@ def test_state_jwt():
     # Arrange
     state = CryptoRandom().getrandomstring(32)
     nonce = CryptoRandom().getrandomstring(32)
-    LaunchDataStorage.set_value(nonce, state)
-    issuer = 'http://localhost:5000'
-    audience = 'http://localhost:2000'
+    issuer = "https://backend.haski.app"
+    audience = "http://localhost:2000"
     expiration = 60
-    claims = {'a' : 'b'}
+    claims = {"a": "b"}
 
     # Act
-    token = jwt.generate_state_jwt(nonce, state, audience, issuer, claims, expiration)
+    token = jwt.generate_state_jwt(
+        nonce, state, audience, issuer, claims, expiration
+    )
+    # When verifying the state token, we need to set the state in the session
+    SessionServiceFlask.set_state_jwt(
+        nonce,
+        "https://moodle.haski.app/mod/lti/auth.php",
+        "https://backend.haski.app",
+    )
     key_public = jwt.load_public_key()
 
     # Assert
-    assert jwt.verify_state_jwt_payload(jwt.verify_jwt(token, key_public))
+    assert jwt.verify_state_jwt_payload(
+        jwt.verify_jwt(token, key_public), verify_nonce=False, session=False
+    )
