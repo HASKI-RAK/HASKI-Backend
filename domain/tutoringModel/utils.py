bee804f7fdf5ad2d85414c79a05fd8a66c9c9780
Yeimy Valencia <yeimy.valencia1@outlook.de>
2023-07-12 14:09:07 +0200

    updated utilities by adding two new Methods

--- domain/tutoringModel/utils.py
+++ domain/tutoringModel/utils.py
@@ -18,6 +18,7 @@ influence = {
 
 # Calculate the coordinates for the LEs
 def get_coordinates(learning_style, list_of_les):
+
     coordinates = {}
     for elememnt in list_of_les:
         if elememnt == cons.abbreviation_ct:
@@ -74,6 +75,7 @@ def get_coordinates(learning_style, list_of_les):
                     learning_style['understanding_value']
                     * influence[elememnt][7])
             coordinates[elememnt] = tuple(coordinate)
+
     return coordinates
 
 
@@ -83,3 +85,56 @@ def distance(xyz1, xyz2) -> float:
         xyz1 = xyz1[1]
         xyz2 = xyz2[1]
     return math.dist(xyz1, xyz2)
+
+
+def get_learning_style( learning_style):
+   
+    result = {}
+
+    str_processing = learning_style.get('processing_dimension')
+    value_processing = learning_style.get('processing_value')    
+    result[str_processing] = value_processing
+
+    str_perception = learning_style.get('perception_dimension')
+    value_perception = learning_style.get('perception_value')    
+    result[str_perception] = value_perception
+
+
+    str_input = learning_style.get('input_dimension')
+    value_input = learning_style.get('input_value')    
+    result[str_input] = value_input
+
+    str_understanding = learning_style.get('understanding_dimension')
+    value_understanding = learning_style.get('understanding_value')    
+    result[str_understanding] = value_understanding
+
+    for key, value in dict(result).items():
+        if value == None:
+            del result[key]
+
+    print("\nnew_learning_style__LS",learning_style )        
+    print("\nnew_Result__LS",result )
+    return result     
+
+
+
+def get_list_learning_element(learning_elements):
+
+    classification_learning_element = []    
+    lz_is_present = False
+    lz_element = ''
+    
+    for le in learning_elements:   
+        
+        if le['classification']=='KÜ':
+            classification_learning_element.insert(0,le['classification'])
+        elif  le['classification']=='LZ':
+            lz_is_present = True
+            lz_element = le['classification']
+        else:    
+            classification_learning_element.append(le['classification'])
+
+    if(lz_is_present):
+        classification_learning_element.append(lz_element)       
+    print("learning elements", classification_learning_element) 
+    return classification_learning_element   
