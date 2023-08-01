56427afda192ae679063e178f6e88922f15930c2
David Fischer <davidvfischer@gmail.com>
2023-08-01 13:55:39 +0200

    datetime import fix

--- entrypoints/flask_app.py
+++ entrypoints/flask_app.py
@@ -1,10 +1,10 @@
-import datetime
 import json
 import os
 import re
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
+from datetime import datetime
 
 import service_layer.crypto.JWTKeyManagement as JWTKeyManagement
 from errors import errors as err
@@ -204,6 +204,7 @@ def course_management(course_id, lms_course_id):
             if condition1 and condition2 and condition3 and condition4:
                 condition5 = re.search(cons.date_format, request.json["last_updated"])
                 if condition5:
+                    last_updated = datetime.strptime(request.json["last_updated"], cons.date_format).date()
                     course = services.update_course(
                         unit_of_work.SqlAlchemyUnitOfWork(),
                         course_id,
@@ -1318,7 +1319,7 @@ def contact_form(user_id, lms_user_id):
         request.json["report_type"],
         request.json["report_topic"],
         request.json["report_description"],
-        datetime.datetime.now(),
+        datetime.now(),
     )
 
     if result is None:
