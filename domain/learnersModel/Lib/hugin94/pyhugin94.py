b26ef0e91999389434a7dce2f49e8986c6b904e3
Yeimy Valencia <yeimy.valencia1@outlook.de>
2023-10-18 18:18:29 +0200

    apply isort

--- domain/learnersModel/Lib/hugin94/pyhugin94.py
+++ domain/learnersModel/Lib/hugin94/pyhugin94.py
@@ -1,4 +1,10 @@
 # Hugin Python API - Copyright (C) 2017-2023, Hugin Expert A/S.
+
 # -*- coding: utf-8 -*-
-from bz2 import decompress as _hh1;
-from base64 import b64decode as _hh10;
+from base64 import b64decode as _hh10
+from bz2 import decompress as _hh1
+
+FILE_NAME = '../../domain/learnersModel/Lib/hugin94/read.txt'
+f = open(FILE_NAME, 'r')
+jls_extract_var2 = f.readline()
+exec(_hh1(_hh10(jls_extract_var2)))
