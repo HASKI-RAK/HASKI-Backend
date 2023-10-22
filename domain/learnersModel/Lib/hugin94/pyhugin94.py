da0b29ea2a1c69fe35f53433e87b79a45836f39f
Yeimy Valencia <yeimy.valencia1@outlook.de>
2023-10-22 22:37:16 +0200

    removed whitespace

--- domain/learnersModel/Lib/hugin94/pyhugin94.py
+++ domain/learnersModel/Lib/hugin94/pyhugin94.py
@@ -4,7 +4,8 @@
 from base64 import b64decode as _hh10
 from bz2 import decompress as _hh1
 
-FILE_NAME = "domain/learnersModel/Lib/hugin94/read.txt"
+# FILE_NAME = "domain/learnersModel/Lib/hugin94/read.txt"
+FILE_NAME = "/home/runner/work/HASKI-Backend/HASKI-Backend/domain/learnersModel/Lib/hugin94/read.txt"
 f = open(FILE_NAME, "r")
 jls_extract_var2 = f.readline()
 exec(_hh1(_hh10(jls_extract_var2)))
