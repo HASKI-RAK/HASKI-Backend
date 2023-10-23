570b537378f5f319169260ad5eb0b673ced8bdb9
Yeimy Valencia <yeimy.valencia1@outlook.de>
2023-10-23 16:09:08 +0200

    cleanup load

--- domain/learnersModel/Lib/hugin94/pyhugin94.py
+++ domain/learnersModel/Lib/hugin94/pyhugin94.py
@@ -5,7 +5,7 @@ from base64 import b64decode as _hh10
 from bz2 import decompress as _hh1
 
 # FILE_NAME = "../../domain/learnersModel/Lib/hugin94/read.txt"
-FILE_NAME = "../domain/learnersModel/Lib/hugin94/read.txt"
+FILE_NAME = "domain/learnersModel/Lib/hugin94/read.txt"
 
 f = open(FILE_NAME, "r")
 jls_extract_var2 = f.readline()
