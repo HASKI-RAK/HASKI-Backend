9f44e4b217e563bb9fbb7a31526309ce57149dd2
MarcNor <marc.normann@rz-online.de>
2023-11-06 11:27:03 +0100

    Updated graf for complexity

--- domain/tutoringModel/graf.py
+++ domain/tutoringModel/graf.py
@@ -1,4 +1,6 @@
 from domain.tutoringModel.utils import check_learning_style, influence
+
+import errors.errors as err
 from utils import constants as cons
 
 
@@ -14,60 +16,78 @@ class GrafAlgorithm:
         self.student_id = student_id
         self.learning_path = learning_path
 
-    def calculate_graf_score(self, learning_element, learning_style):
+    def get_standard_values_for_graf(self, learning_element):
         if (learning_element == cons.abbreviation_ct):
             return 99
         elif (learning_element == cons.abbreviation_co):
             return 98
         elif (learning_element == cons.abbreviation_as):
             return -99
-        elif (learning_element == cons.abbreviation_cc):
+
+    def get_cc_score(self, learning_style):
+        if (
+            learning_style["processing_dimension"] == "ref"
+            and learning_style["understanding_dimension"] == "seq"
+        ):
             if (
-                learning_style["processing_dimension"] == "ref"
-                and learning_style["understanding_dimension"] == "seq"
-            ):
-                if (
-                    learning_style["processing_value"]
-                    > learning_style["understanding_value"]
-                ):
-                    return 97
-                else:
-                    return 0
-            elif (
-                learning_style["processing_dimension"] == "ref"
-                or learning_style["understanding_dimension"] == "glo"
+                learning_style["processing_value"]
+                > learning_style["understanding_value"]
             ):
                 return 97
             else:
                 return 0
+        elif (
+            learning_style["processing_dimension"] == "ref"
+            or learning_style["understanding_dimension"] == "glo"
+        ):
+            return 97
         else:
-            sum = 0
-            print(influence[learning_element])
-            if (learning_style["processing_dimension"] == "act"):
-                sum = sum + influence[learning_element][0] * \
-                    learning_style["processing_value"]
-            else:
-                sum = sum + influence[learning_element][1] * \
-                    learning_style["processing_value"]
-            if (learning_style["perception_dimension"] == "sns"):
-                sum = sum + influence[learning_element][2] * \
-                    learning_style["perception_value"]
-            else:
-                sum = sum + influence[learning_element][3] * \
-                    learning_style["perception_value"]
-            if (learning_style["input_dimension"] == "act"):
-                sum = sum + influence[learning_element][4] * \
-                    learning_style["input_value"]
-            else:
-                sum = sum + influence[learning_element][5] * \
-                    learning_style["input_value"]
-            if (learning_style["understanding_dimension"] == "act"):
-                sum = sum + influence[learning_element][6] * \
-                    learning_style["understanding_value"]
-            else:
-                sum = sum + influence[learning_element][7] * \
-                    learning_style["understanding_value"]
-            return sum
+            return 0
+
+    def calculate_variable_score(self, learning_element, learning_style):
+        score = 0
+        found = False
+        if (learning_style["processing_dimension"] == "act"):
+            score = score + influence[learning_element][0] * \
+                learning_style["processing_value"]
+        else:
+            score = score + influence[learning_element][1] * \
+                learning_style["processing_value"]
+        if (learning_style["perception_dimension"] == "sns"):
+            score = score + influence[learning_element][2] * \
+                learning_style["perception_value"]
+        else:
+            score = score + influence[learning_element][3] * \
+                learning_style["perception_value"]
+        if (learning_style["input_dimension"] == "act"):
+            score = score + influence[learning_element][4] * \
+                learning_style["input_value"]
+        else:
+            score = score + influence[learning_element][5] * \
+                learning_style["input_value"]
+        if (learning_style["understanding_dimension"] == "act"):
+            score = score + influence[learning_element][6] * \
+                learning_style["understanding_value"]
+        else:
+            score = score + influence[learning_element][7] * \
+                learning_style["understanding_value"]
+        if found:
+            return score
+        else:
+            raise err.WrongLearningElementError()
+
+    def calculate_graf_score(self, learning_element, learning_style):
+        if(
+            learning_element == cons.abbreviation_ct or
+            learning_element == cons.abbreviation_co or
+            learning_element == cons.abbreviation_as
+        ):
+            return self.get_standard_values_for_graf(learning_element)
+        elif (learning_element == cons.abbreviation_cc):
+            return self.get_cc_score(learning_style)
+        else:
+            return self.calculate_variable_score(learning_element, learning_style)
+        
 
     def sort_learning_path(self, learning_path):
         sort_learning_path = []
