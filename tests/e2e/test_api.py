0d8b81a1b0538b80c595ef98d33077c54a315aa7
DimitriB01 <d.bigler1996@gmx.de>
2024-07-15 14:19:27 +0200

    added test to update course_start

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -3111,6 +3111,65 @@ class TestApi:
         for key in keys_expected:
             assert key in response.keys()
 
+    @pytest.mark.parametrize(
+        "input, moodle_course_id, keys_expected,\
+                            status_code_expected",
+        [
+            # Working Example
+            (
+                    {
+                        "name": "Test Course Updated",
+                        "created_by": "Maria Musterfrau",
+                        "created_at": "2023-08-01T13:37:42Z",
+                        "last_updated": "2018-07-21T17:32:28Z",
+                        "university": "TH-AB",
+                        "start_date": "2023-08-01T13:37:42Z",
+                    },
+                    1,
+                    ["id", "name", "lms_id", "created_at", "created_by", "university",\
+                     "start_date"],
+                    201,
+            ),
+            # Missing Parameter
+            (
+                    {
+                        "created_by": "Maria Musterfrau",
+                        "created_at": "2023-08-01T13:37:42Z",
+                        "last_updated": "2018-07-21T17:32:28Z",
+                        "university": "TH-AB",
+                        "start_date": "2023-08-01T13:37:42Z",
+                    },
+                    1,
+                    ["error", "message"],
+                    400,
+            ),
+            # Parameter with wrong data type
+            (
+                    {
+                        "name": "Test Course Updated",
+                        "created_by": "Maria Musterfrau",
+                        "created_at": "2023-08-01T13:37:42Z",
+                        "last_updated": "01.01.2023",
+                        "university": "TH-AB",
+                        "start_date": "what is this?"
+                    },
+                    1,
+                    ["error", "message"],
+                    400,
+            ),
+        ],
+    )
+    def test_update_course_from_moodle_with_start_date(
+            self, client_class, input, moodle_course_id, keys_expected, status_code_expected
+    ):
+        global course_id
+        url = path_lms_course + "/" + str(course_id) + "/" + str(moodle_course_id)
+        r = client_class.put(url, json=input)
+        assert r.status_code == status_code_expected
+        response = json.loads(r.data.decode("utf-8").strip("\n"))
+        for key in keys_expected:
+            assert key in response.keys()
+
     # Update a Topic from Moodle
     @pytest.mark.parametrize(
         "input, moodle_course_id, moodle_topic_id,\
