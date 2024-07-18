6596d63771cb92c5ba73158755f19c3483a4954e
DimitriB01 <d.bigler1996@gmx.de>
2024-07-18 08:59:01 +0200

    flaked8, isort, black

--- tests/unit/lti/test_OIDCLoginFlask.py
+++ tests/unit/lti/test_OIDCLoginFlask.py
@@ -1,14 +1,13 @@
+import datetime
 import os
 import unittest
 from unittest import mock
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock, patch
 
 import service_layer.lti.config.ToolConfigJson as ToolConfigJson
 from errors import errors as err
-from service_layer import unit_of_work
 from service_layer.crypto import JWTKeyManagement
 from service_layer.lti.OIDCLoginFlask import OIDCLoginFlask
-from service_layer.service import SessionServiceFlask
 
 # Test configuration data
 config_file = {
@@ -30,6 +29,7 @@ config_file = {
                     "alg": "RS256",
                     "kid": "5e58bef6fa8030be050b",
                     "e": "AQAB",
+                    # pylint: disable=line-too-long
                     "n": "z8uwIpHep-dGbqqutVqQPsXdwJk8ESoXPw1UD-9PmlTm06Q6PmXl5jHT9J6hTuW-9OjiycuBs07DW1At6LEAEBOJbFw2H6aflmPyhq09Cwernuk6OzekRRmnPdmfYeOcjpHAGaZ3qwaU0E6zPt_Ki1ZqdtLnB53ytO1fuYTmK1FVSbnexK9i4OkLk6OHMCHkInQTndRMWOiKWwrAoc591LlNSzgvlW_S9s-Vj2N4sHAcikKobzMMD8ixsV84Lx3mZyb13qph9qXUMJB9It-5WXa-FfsbUQZ6MeC0ks9_XSyl7tI7Q2eHUqenPimZL15uQnWek-gaE61IAK7ihKSMew",
                     "use": "sig",
                 }
@@ -51,6 +51,7 @@ form = {
     "target_link_uri": "https://backend.ke.haski.app/lti_launch",
     "lti_deployment_id": "1",
 }
+
 
 # Test cases for OIDCLoginFlask
 class TestOIDCLoginFlask(unittest.TestCase):
@@ -75,13 +76,13 @@ class TestOIDCLoginFlask(unittest.TestCase):
             mock_form.form = form.copy()
             mock_form.host = host
             with patch.object(
-                    ToolConfigJson,
-                    "decode_platform",
-                    return_value=None,
+                ToolConfigJson,
+                "decode_platform",
+                return_value=None,
             ):
                 with self.assertRaisesRegex(
-                        err.ErrorException,
-                        "No platform found",
+                    err.ErrorException,
+                    "No platform found",
                 ):
                     self.oidc_login.check_params()
 
@@ -98,19 +99,19 @@ class TestOIDCLoginFlask(unittest.TestCase):
             mock_form.form = form.copy()
             mock_form.form.pop("iss", None)
             with self.assertRaisesRegex(
-                    err.ErrorException, "Missing parameters in request"
+                err.ErrorException, "Missing parameters in request"
             ):
                 self.oidc_login.check_params()
 
     def test_check_params_missing_platform(self):
         with patch.object(
-                ToolConfigJson,
-                "get_platform",
-                return_value=None,
+            ToolConfigJson,
+            "get_platform",
+            return_value=None,
         ):
             with patch.object(self.oidc_login, "_request", _request=MagicMock):
                 with self.assertRaisesRegex(
-                        err.ErrorException, "Missing parameters in request"
+                    err.ErrorException, "Missing parameters in request"
                 ):
                     self.oidc_login.check_params()
 
@@ -119,7 +120,7 @@ class TestOIDCLoginFlask(unittest.TestCase):
             mock_form.form = form.copy()
             mock_form.form["target_link_uri"] = "wrong_uri"
             with self.assertRaisesRegex(
-                    err.ErrorException, "target_link_uri is not from the same host"
+                err.ErrorException, "target_link_uri is not from the same host"
             ):
                 self.oidc_login.check_params()
 
@@ -131,11 +132,11 @@ class TestOIDCLoginFlask(unittest.TestCase):
             mock_form.host = host
             mock_form.form["target_link_uri"] = "http://backend.haski.app/lti_launch"
             with patch.dict(
-                    os.environ,
-                    {"FLASK_ENV": "production"},
+                os.environ,
+                {"FLASK_ENV": "production"},
             ):
                 with self.assertRaisesRegex(
-                        err.ErrorException, "target_link_uri is not HTTPS"
+                    err.ErrorException, "target_link_uri is not HTTPS"
                 ):
                     self.oidc_login.check_params()
 
@@ -146,12 +147,12 @@ class TestOIDCLoginFlask(unittest.TestCase):
             mock_form.form["state"] = "valid_state_jwt"
 
             with patch(
-                    "service_layer.crypto.JWTKeyManagement.verify_jwt"
+                "service_layer.crypto.JWTKeyManagement.verify_jwt"
             ) as mock_verify_jwt:
                 mock_verify_jwt.return_value = {"nonce": "valid_nonce"}
 
                 with patch(
-                        "service_layer.crypto.JWTKeyManagement.verify_state_jwt_payload"
+                    "service_layer.crypto.JWTKeyManagement.verify_state_jwt_payload"
                 ) as mock_verify_state_jwt_payload:
                     mock_verify_state_jwt_payload.return_value = True
 
@@ -169,12 +170,12 @@ class TestOIDCLoginFlask(unittest.TestCase):
             mock_form.form["state"] = "invalid_state_jwt"
 
             with patch(
-                    "service_layer.crypto.JWTKeyManagement.verify_jwt"
+                "service_layer.crypto.JWTKeyManagement.verify_jwt"
             ) as mock_verify_jwt:
                 mock_verify_jwt.side_effect = err.InvalidJWTError()
 
                 with self.assertRaisesRegex(
-                        err.InvalidJWTError, "Invalid state signature"
+                    err.InvalidJWTError, "Invalid state signature"
                 ):
                     self.oidc_login.verify_state()
 
@@ -187,13 +188,13 @@ class TestOIDCLoginFlask(unittest.TestCase):
             mock_form.form["state"] = "invalid_state_jwt"
 
             with patch(
-                    "service_layer.crypto.JWTKeyManagement.verify_jwt"
+                "service_layer.crypto.JWTKeyManagement.verify_jwt"
             ) as mock_verify_jwt:
                 mock_verify_jwt.return_value = {"nonce": "invalid_nonce"}
 
                 with patch.object(
-                        JWTKeyManagement,
-                        "verify_state_jwt_payload",
+                    JWTKeyManagement,
+                    "verify_state_jwt_payload",
                 ) as mock_verify_state_jwt_payload:
                     mock_verify_state_jwt_payload.side_effect = err.InvalidJWTError()
 
@@ -221,12 +222,12 @@ class TestOIDCLoginFlask(unittest.TestCase):
             mock_form.form["id_token"] = "valid_id_token_jwt"
 
             with patch(
-                    "service_layer.crypto.JWTKeyManagement.verify_jwt"
+                "service_layer.crypto.JWTKeyManagement.verify_jwt"
             ) as mock_verify_jwt:
                 mock_verify_jwt.return_value = {"sub": "valid_sub"}
 
                 with self.assertRaisesRegex(
-                        err.InvalidJWTError, "Error loading header"
+                    err.InvalidJWTError, "Error loading header"
                 ):
                     self.oidc_login.verify_id_token()
 
@@ -239,28 +240,28 @@ class TestOIDCLoginFlask(unittest.TestCase):
             mock_form.form["id_token"] = "valid_id_token_jwt"
 
             with patch.object(
-                    JWTKeyManagement,
-                    "get_unverified_header",
-                    return_value={
-                        "kid": config_file["https://moodle.haski.app"]["key_set"]["keys"][
-                            0
-                        ]["kid"],
-                    },
+                JWTKeyManagement,
+                "get_unverified_header",
+                return_value={
+                    "kid": config_file["https://moodle.haski.app"]["key_set"]["keys"][
+                        0
+                    ]["kid"],
+                },
             ):
                 with patch(
-                        "service_layer.crypto.JWTKeyManagement.load_jwt",
+                    "service_layer.crypto.JWTKeyManagement.load_jwt",
                 ) as mock_load_jwt:
                     mock_load_jwt.return_value = {
                         "sub": "valid_sub",
                         "iss": "https://moodle.haski.app",
                     }
                     with patch.object(
-                            ToolConfigJson,
-                            "get_platform",
-                            return_value=config_file["https://moodle.haski.app"],
+                        ToolConfigJson,
+                        "get_platform",
+                        return_value=config_file["https://moodle.haski.app"],
                     ):
                         with patch(
-                                "service_layer.crypto.JWTKeyManagement.verify_jwt"
+                            "service_layer.crypto.JWTKeyManagement.verify_jwt"
                         ) as mock_verify_jwt:
                             mock_verify_jwt.return_value = {
                                 "sub": "valid_sub",
@@ -274,24 +275,39 @@ class TestOIDCLoginFlask(unittest.TestCase):
                             )
 
     def test_get_logout(self):
-        with patch.object(self.oidc_login, '_request', _request=MagicMock) as mock_request:
-            mock_request.referrer = 'https://example.com'
+        with patch.object(
+            self.oidc_login, "_request", _request=MagicMock
+        ) as mock_request:
+            mock_request.referrer = "https://example.com"
 
             response = self.oidc_login.get_logout()
 
             self.assertEqual(response.status_code, 204)
-            set_cookie_header = response.headers.get('Set-Cookie', '')
-            self.assertIn('haski_state=', set_cookie_header)  # Check for empty value
-            self.assertIn('Max-Age=0', set_cookie_header)
-            self.assertIn('Domain=example.com', set_cookie_header)
-            self.assertIn('HttpOnly', set_cookie_header)
-            self.assertIn('SameSite=Lax', set_cookie_header)
-            self.assertIn('Path=/', set_cookie_header)
+            set_cookie_header = response.headers.get("Set-Cookie", "")
+            self.assertIn("haski_state=", set_cookie_header)  # Check for empty value
+            self.assertIn("Max-Age=0", set_cookie_header)
+            self.assertIn("Domain=example.com", set_cookie_header)
+            self.assertIn("HttpOnly", set_cookie_header)
+            self.assertIn("SameSite=Lax", set_cookie_header)
+            self.assertIn("Path=/", set_cookie_header)
 
-            expires_part = next((part for part in set_cookie_header.split('; ') if part.startswith('Expires=')), None)
-            self.assertIsNotNone(expires_part, "Expires attribute not found in Set-Cookie header")
-            expires_date = expires_part.split('=', 1)[1]
+            expires_part = next(
+                (
+                    part
+                    for part in set_cookie_header.split("; ")
+                    if part.startswith("Expires=")
+                ),
+                None,
+            )
+            self.assertIsNotNone(
+                expires_part, "Expires attribute not found in Set-Cookie header"
+            )
+            expires_date = expires_part.split("=", 1)[1]
             from email.utils import parsedate_to_datetime
-            import datetime
+
             expires_datetime = parsedate_to_datetime(expires_date)
-            self.assertLess(expires_datetime, datetime.datetime.now(datetime.timezone.utc), "Expires date should be in the past")
+            self.assertLess(
+                expires_datetime,
+                datetime.datetime.now(datetime.timezone.utc),
+                "Expires date should be in the past",
+            )
