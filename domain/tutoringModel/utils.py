6cf0d8800f303035ebd7a5bb8b81716c7049176c
Yeimy Valencia <yeimy.valencia1@outlook.de>
2025-07-08 12:50:51 +0200

    add new dimensions and Normalize function

--- domain/tutoringModel/utils.py
+++ domain/tutoringModel/utils.py
@@ -92,6 +92,74 @@ def get_coordinates(learning_style, list_of_les):
     return coordinates
 
 
+
+# Calculate the coordinates for the LEs
+def get_coordinates_plus(learning_style, list_of_les):
+    coordinates = {}
+    for elememnt in list_of_les:
+        if elememnt == cons.abbreviation_ct:
+            coordinates[cons.abbreviation_ct] = (13, 13, 13, 13, 13, 13)
+        elif elememnt == cons.abbreviation_co:
+            coordinates[cons.abbreviation_co] = (12, 12, 12, 12, 12, 12)
+        elif elememnt == cons.abbreviation_as:
+            coordinates[cons.abbreviation_as] = (-12, -12, -12, -12, -12, -12)
+        elif elememnt == cons.abbreviation_cc:
+            if (
+                learning_style["processing_dimension"] == "ref"
+                and learning_style["understanding_dimension"] == "seq"
+            ):
+                if (
+                    learning_style["processing_value"]
+                    > learning_style["understanding_value"]
+                ):
+                    coordinates[cons.abbreviation_cc] = (11, 11, 11, 11, 11, 11)
+                else:
+                    coordinates[cons.abbreviation_cc] = (0, 0, 0, 0, 0, 0)
+            elif (
+                learning_style["processing_dimension"] == "ref"
+                or learning_style["understanding_dimension"] == "glo"
+            ):
+                coordinates[cons.abbreviation_cc] = (11, 11, 11, 11, 11, 11)
+            else:
+                coordinates[cons.abbreviation_cc] = (0, 0, 0, 0, 0, 0)
+        else:
+            coordinate = list()
+            if learning_style["processing_dimension"] == "act":
+                coordinate.append(
+                    learning_style["processing_value"] * influence[elememnt][0]
+                )
+            elif learning_style["processing_dimension"] == "ref":
+                coordinate.append(
+                    learning_style["processing_value"] * influence[elememnt][1]
+                )
+            if learning_style["perception_dimension"] == "sns":
+                coordinate.append(
+                    learning_style["perception_value"] * influence[elememnt][2]
+                )
+            elif learning_style["perception_dimension"] == "int":
+                coordinate.append(
+                    learning_style["perception_value"] * influence[elememnt][3]
+                )
+            if learning_style["input_dimension"] == "vis":
+                coordinate.append(
+                    learning_style["input_value"] * influence[elememnt][4]
+                )
+            elif learning_style["input_dimension"] == "vrb":
+                coordinate.append(
+                    learning_style["input_value"] * influence[elememnt][5]
+                )
+            if learning_style["understanding_dimension"] == "seq":
+                coordinate.append(
+                    learning_style["understanding_value"] * influence[elememnt][6]
+                )
+            elif learning_style["understanding_dimension"] == "glo":
+                coordinate.append(
+                    learning_style["understanding_value"] * influence[elememnt][7]
+                )
+            coordinates[elememnt] = tuple(coordinate)
+    return coordinates
+
+
 # Euclidean Distance Function
 def distance(xyz1, xyz2) -> float:
     if isinstance(xyz1[0], str):
@@ -203,3 +271,11 @@ def get_learning_path_as_str(result_ga):
         str_learning_path = str_learning_path[:-2]
 
     return str_learning_path
+
+
+
+def normalize_array(data, new_min=-12, new_max=13):
+    old_min = np.min(data)
+    old_max = np.max(data)
+    return new_min + (data - old_min) * (new_max - new_min) / (old_max - old_min)
+
