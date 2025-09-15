98db207c8fe4207beb5c1250728f485d71237e52
DimitriB01 <d.bigler1996@gmx.de>
2025-09-15 13:13:35 +0200

    wrong id in delete learning Element (in delete course)

--- entrypoints/flask_app.py
+++ entrypoints/flask_app.py
@@ -303,14 +303,14 @@ def course_administration(data: Dict[str, Any], course_id, lms_course_id):
                 )
                 for learning_element in learning_elements:
                     services.delete_student_learning_element_by_learning_element_id(
-                        unit_of_work.SqlAlchemyUnitOfWork(), learning_element["id"]
+                        unit_of_work.SqlAlchemyUnitOfWork(), learning_element["learning_element_id"]
                     )
                     services.delete_learning_element_solution(
                         unit_of_work.SqlAlchemyUnitOfWork(),
                         learning_element["learning_element_id"],
                     )
                     services.delete_learning_element(
-                        unit_of_work.SqlAlchemyUnitOfWork(), learning_element["id"]
+                        unit_of_work.SqlAlchemyUnitOfWork(), learning_element["learning_element_id"]
                     )
                 services.delete_topic(unit_of_work.SqlAlchemyUnitOfWork(), topic["id"])
             services.delete_course(unit_of_work.SqlAlchemyUnitOfWork(), course_id)
